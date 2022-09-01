82a04aff2565a90473613128792c277baa5fd2db
Panchout <nathanpanchout@live.fr>
2022-09-01 18:11:29 +0200

    wip

--- src/components/Program/Card/ProgramCard.tsx
+++ src/components/Program/Card/ProgramCard.tsx
@@ -12,7 +12,7 @@ export const ProgramCard: FunctionComponent<ProgramCardProps> = ({...props}) => 
         <div className='relative'>
             <img alt='Affiche du programme' width={200} height={266} className='rounded-[8px]' src={props.program.thumnail.url}/>
             <div className='cursor-pointer bg-black/[.7] absolute rounded-[8px] text-white bottom-[6px] right-[7px] w-[32px] h-[32px] flex justify-center items-center'>
-                <img width={16} height={16} src='/images/add.svg' alt={'add icon '}/>
+                <img width={16} height={16} src='/images/add.svg' alt={'add icon'}/>
             </div>
         </div>
         <div className='line-clamp-2 px-2 text-center' dangerouslySetInnerHTML={{__html: props.program.name}}/>
