9f61a25ae9a1e8070d98b38507f7ab55e8596db4
Panchout <nathanpanchout@live.fr>
2022-09-01 17:39:11 +0200

    first commit

---
+++ src/components/Program/Card/ProgramCard.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {FunctionComponent} from "react";
+
+interface ProgramCardProps {
+}
+
+export const ProgramCard: FunctionComponent<ProgramCardProps> = ({...props}) => {
+    
+    return <div></div>
+}
---
+++ src/pages/Programs/ProgramsPage.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {FunctionComponent} from "react";
+
+interface ProgramsPageProps {
+}
+
+export const ProgramsPage: FunctionComponent<ProgramsPageProps> = ({...props}) => {
+    
+    return <div></div>
+}
